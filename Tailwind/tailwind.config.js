43e3d557b7ae2af013b4905b5b3ceeab155d3d44
wala-barhoumi <walabarhoumi081@gmail.com>
2024-05-19 18:20:44 +0100

    tailwind

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'my-blue': '#007bff',
+      },
+    },
+  },
+  plugins: [],
+}
+
